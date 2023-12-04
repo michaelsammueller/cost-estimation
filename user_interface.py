c07241ba99a79ad1cda809d75c5f5e18202ea02d
TarekHamzaGoda <106682878+TarekHamzaGoda@users.noreply.github.com>
2023-12-04 20:09:54 +0400

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -2,6 +2,8 @@ from cmd import *  # for importing cmd inputs
 import os
 import json
 import pandas as pd
+from calculation import ProjectEstimator
+
 
 data = {}
 files = []  # list for file selection
@@ -9,6 +11,11 @@ files = []  # list for file selection
 
 class UserInputs(Cmd):
     """Class to contain main terminal options by using the do_ commands to record user input"""
+
+    def __init__(self):
+        """Class to handle cli commands"""
+        super().__init__()
+        self.estimator = ProjectEstimator()
 
     def do_ext(self, *args):
         """Closes the program"""
@@ -33,7 +40,7 @@ class UserInputs(Cmd):
             # syntax used to open json file
             with open(f'data/{file_name_input}', 'r') as file1:
                 data = json.load(file1)
-                print(data)
+                #print(data)
                 print("Json File Loaded")  # test if the file is loaded
 
         elif file_name_input == "ext":
@@ -54,14 +61,16 @@ class UserInputs(Cmd):
         # Using Pandas dataframe method to display the tables in the terminal
         hw_table = pd.DataFrame([[value for key, value in item.items()] for item in hardware],
                                 columns=["Type", "Description", "Count", "Price", "Mfg. Cost", "Design Cost",
-                                         "Coding Cost", "Testing Cost"])
+                                         "Coding Cost", "Testing Cost", "skill_1_needed", "skill_2_needed"])
 
         sw_table = pd.DataFrame([[value for key, value in item.items()] for item in software],
                                 columns=["Type", "Description", "Count", "Price", "Mfg. Cost", "Design Cost",
-                                         "Coding Cost", "Testing Cost"])
+                                         "Coding Cost", "Testing Cost", "lines_of_code",
+                                         "skill_1_needed", "skill_2_needed",
+                                         "skill_3_needed"])
 
         res_table = pd.DataFrame([[value for key, value in item.items()] for item in resources],
-                                 columns=["role", "count", "cost", "days"])
+                                 columns=["role", "type", "count", "cost", "days", "skill_1", "skill_2", "skill_3"])
 
         print(20 * "-", " Hardware Components", 20 * "-")
         print(hw_table)
@@ -70,6 +79,21 @@ class UserInputs(Cmd):
         print(20 * "-", " Resources", 20 * "-")
         print(res_table)
 
+    def do_clc(self, *args):
+        """A function for calculating the .json data using the calculation .py """
+
+        self.estimator.clear()
+        self.estimator.read_json_data(data)
+
+        # gets calculation methods from calculator
+        grand_total = self.estimator.total_system_cost()
+        print(f"Grand_Total = {grand_total}")
+        system_cost = self.estimator.cost_per_system()
+        print(f"System_Cost = {system_cost}")
+        cocomo_cost = self.estimator.cocomo_estimation("Organic")
+        print(f"cocomo_cost = {cocomo_cost}")
+        actual_staff_cost = self.estimator.total_staff_cost()
+        print(f"Actual_Staff_Cost = {actual_staff_cost}")
 
 
 def main_menu():
